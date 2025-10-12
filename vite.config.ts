31bfa6f89f5fd3675c69e30a32154b3d9a6925ee
Andrey Parkhomenko <Andrey.Parkhomenko@tele2.kz>
2025-10-12 21:27:51 +0500

    add changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/v-d', 
-  server: {
-    host: 'localhost', // можно '0.0.0.0' если нужен доступ с других устройств
-    port: 3000,
-    strictPort: true,  // если порт занят — упадёт с ошибкой, не прыгнет на 3001+
-  },
-  preview: {
-    host: 'localhost',
-    port: 3000,
-    strictPort: true,
-  },
+  base: '/v-d/', 
 })
